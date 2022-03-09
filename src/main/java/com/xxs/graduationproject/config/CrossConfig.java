bf4e3bdec533a89cf767b78654d1090581a98d70
xiongxiaosong <xxs00041426@outlook.com>
2022-03-09 23:58:55 +0800

    11.导入邮件依赖，编写邮件发送工具类，基本配置

--- src/main/java/com/xxs/graduationproject/config/CrossConfig.java
+++ src/main/java/com/xxs/graduationproject/config/CrossConfig.java
@@ -3,14 +3,14 @@ package com.xxs.graduationproject.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//下面是springboot的跨域配置类，springboot版本 必须是 2.4 以前的版本
 @Configuration
 public class CrossConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")/*所有的当前站点的请求地址，都支持跨域访问*/
-                .allowedOriginPatterns("*")/* springboot新版本方法*/
+                .allowedOriginPatterns("*")/*  2.6版本修改方法*/
                 //.allowedOrigins("*")/*所有的外部域都可跨域访问*/
                 .allowedMethods("GET","HEAD","POST","PUT","DELETE","OPTIONS")/*哪些请求 需要跨域配置*/
                 .allowCredentials(true) /*是否支持跨域用户凭证*/
